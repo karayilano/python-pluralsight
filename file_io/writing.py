dc8ded84aee362af1c9c0e995b9bff32ed92bde9
Oğuzhan Kürşat Karayılan <56203702+karayilano@users.noreply.github.com>
2022-10-07 11:20:07 +0300

    Add files via upload

---
+++ file_io/writing.py
@@ -0,0 +1,9 @@
+
+
+f = open('wasteland.txt', mode='wt', encoding='utf-8')
+
+f.write("Bu bir şiir\n")
+f.write("Selam sana güneş\n")
+f.write("Doğdun ne güzel güneş\n")
+f.write("Eskiden basitmiş\n")
+f.close()
